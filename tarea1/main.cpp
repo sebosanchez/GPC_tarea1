8775a5224cad6b9e6e392305efa66997c3bc69dc
Sebastian Alcala <sebastiansanchez@MacBook-Pro-de-Sebastian.local>
2017-08-31 15:43:07 -0500

    Initial Commit

---
+++ tarea1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea1
+//
+//  Created by Sebastian Sanchez Alcala on 31/08/17.
+//  Copyright © 2017 Sebastian Sanchez Alcala. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
