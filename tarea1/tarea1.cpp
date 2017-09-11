7af781a4469892a5fda81ffa9616cc9175317848
lpantojaa <lmpantoja@hotmail.com>
2017-09-11 17:24:21 -0500

    Ejercicios 1, 5, 6

--- tarea1/tarea1.cpp
+++ tarea1/tarea1.cpp
@@ -80,8 +80,76 @@ public:
     }
     
     // EJER 1) operador de entrada del clsVector3D:
-//    friend istream& operator >>( istream& is, clsVector3D& v ){
-//    }
+friend istream& operator >>(istream& is, clsVector3D& v)
+{
+        v.leeVector(&is);
+        return is;
+	}
+
+	void leeVector(istream *f)
+	{
+		enum edo { S0, S1, S2, S3, SF, SError };
+		enum edo s = S0;
+		char c;
+		string n;
+
+		while ((s != SF) && (s != SError)){
+			do (*f).get(c); while (c == ' ');
+			if ((*f).eof()) c = '\0';
+
+			switch (s){
+
+			case S0:if (c == '(')
+				s = S1;
+					else
+						s = SError;
+				break;
+
+			case S1:
+				if (('0' <= c  && c <= '9') || c=='-')
+					n.push_back(c);
+				else
+                    if(c==','){
+                        dbl_x=stod(n);
+                        n="";
+                        s = S2;
+                    }
+                    else
+                        s = SError;
+				break;
+
+			case S2:if (('0' <= c  && c <= '9')|| c=='-')
+                    n.push_back(c);
+				else
+                    if(c==','){
+                        dbl_y=stod(n);
+                        n="";
+                        s = S3;
+                    }
+                    else
+                        s = SError;
+				break;
+
+			case S3:if (('0' <= c  && c <= '9')|| c=='-')
+					n.push_back(c);
+				else
+                    if(c==')'){
+                        dbl_z=stod(n);
+                        n="";
+                        s = SF;
+                    }
+                    else
+                        s = SError;
+				break;
+			}
+		}
+
+		if (s == SF)
+			cout << "Vector Leido\n";
+		else
+			cout << "Error en la cadena\n";
+	}
+	
     // EJER 2) operador Producto Cruz de dos clsVector3D
 //    ya está
     friend clsVector3D operator *( clsVector3D& a, clsVector3D& b )
@@ -134,34 +202,146 @@ public:
         string x = (p.B()<0) ? "x " : "x +";
         string y = (p.C()<0) ? "y " : "y +";
         string z = (p.D()<0) ? "z " : "z +";
-        cout << "plano: " << p.A() << x << p.B() << y << p.C() << z << p.D() << " = 0\n";
+        os << "plano: " << p.A() << x << p.B() << y << p.C() << z << p.D() << " = 0\n";
         return os;
     }
     
     // EJER 5) Lectura del plano como A * x + B * y + c * z + D = 0.
-    friend istream& operator >>( istream& is, clsPlano3D& p )
-    {
-        clsPlano3D w;
-        p = w;
-        return is;
-    }
+   friend istream& operator >>(istream& is, clsPlano3D& p)
+   {
+        p.leePlano(&is);
+		return is;
+	}
+
+	void leePlano(istream *f)
+	{
+		enum edo { S0, S1, S2, S3, SF, SError };
+		enum edo s = S0;
+		char c;
+		string pl;
+
+		while ((s != SF) && (s != SError)){
+			do (*f).get(c); while (c == ' ');
+			if ((*f).eof()) c = '\0';
+
+			switch (s){
+
+			case S0:
+			    if (('0' <= c  && c <= '9') || c=='-')
+				pl.push_back(c);
+				else
+                    if(c=='x' || c=='X'){
+                        dbl_A=stod(pl);
+                        pl="";
+                        s = S1;
+                    }
+                    else
+                        if (c=='*' || c=='+')
+                            ;
+                        else
+                            s = SError;
+				break;
+
+			case S1:
+				if (('0' <= c  && c <= '9') || c=='-' || c=='+')
+					pl.push_back(c);
+				else
+                    if(c=='y' || c=='Y'){
+                        dbl_B=stod(pl);
+                        pl="";
+                        s = S2;
+                    }
+                    else
+                        if (c=='*')
+                            ;
+                        else
+                            s = SError;
+				break;
+
+			case S2:
+			    if (('0' <= c  && c <= '9') || c=='-')
+					pl.push_back(c);
+				else
+                    if(c=='z' || c=='Z'){
+                        dbl_C=stod(pl);
+                        pl="";
+                        s = S3;
+                    }
+                    else
+                        if (c=='*' || c=='+')
+                            ;
+                        else
+                            s = SError;
+				break;
+
+			case S3:
+			    if (('0' <= c  && c <= '9') || c=='-')
+					pl.push_back(c);
+				else
+                    if(c=='='){
+                        dbl_D=stod(pl);
+                        pl="";
+                        s = SF;
+                    }
+                    else
+                        if (c=='+')
+                            ;
+                        else
+                            s = SError;
+				break;
+			}
+		}
+		if (s == SF)
+			cout << "Plano Leido\n";
+		else
+			cout << "Error en la cadena\n";
+	}
     
     // EJER 6) Obtener el punto que resulta de la intersecciÛn de tres planos
     friend clsVector3D interseccion( clsPlano3D P1, clsPlano3D P2, clsPlano3D P3 )
     {
-        // va el dummy con el ( 100.0 ,200.0, 300.0)
-        clsVector3D a;
-        a.x( 100.0 );
-        a.y( 200.0 );
-        a.z( 300.0 );
-        return a;
+		clsVector3D a;
+		double planos[3][3]={{P1.A(),P1.B(),P1.C()},{P2.A(),P2.B(),P2.C()},{P3.A(),P3.B(),P3.C()}};
+		double aux1[3][3];
+		double aux2[3][3];
+		double aux3[3][3];
+		double d[3]={P1.D(),P2.D(),P3.D()};
+		double detP=det(planos);
+
+		for(int j=0;j<3;j++)
+            for(int k=0;k<3;k++){
+                aux1[j][k]=planos[j][k];
+                aux2[j][k]=planos[j][k];
+                aux3[j][k]=planos[j][k];
+            }
+
+        for(int i=0;i<3;i++){
+            aux1[i][0]=-d[i];
+            aux2[i][1]=-d[i];
+            aux3[i][2]=-d[i];
+        }
+        a.x(det(aux1)/detP);
+        a.y(det(aux2)/detP);
+        a.z(det(aux3)/detP);
+
+		return a;
     }
+	
+	static double det(double m[3][3])
+		{
+        return ((m[0][0]*m[1][1]*m[2][2])+(m[0][1]*m[1][2]*m[2][0])+(m[0][2]*m[1][0]*m[2][1]))-
+        ((m[0][2]*m[1][1]*m[2][0])+(m[0][1]*m[1][0]*m[2][2])+(m[0][0]*m[1][2]*m[2][1]));
+	}
 };
 // =============================================
 int main()
 {
-    clsVector3D a,b,c;
-    
+    clsVector3D a,b,c,d;
+     //Ejercicio 1
+    istrstream sin("(-36,15,12)");
+    sin>>a;
+    cout<<a<<endl;
+	
     a.x( 1.0 );
     a.y( 2.0 );
     a.z( 3.0 );
@@ -171,23 +351,11 @@ int main()
     b.z( -7.0 );
     
     c= a * b;
+	cout<<c;
     
-    //cout << "producto cruz:" << c.x() << "," << c.y() << "," << c.z();
     
-    //cout << a << " + " << b << " = " << a + b << endl;
-    
-    //cout << 5.0 << " * " << a << " = " << 5.0 * a << endl;
-    
-    //cout << a << " * " << 5.0 << " = " << a * 5.0 << endl;
-    
-//    cin >> c;
-    
-//    cout << "c = " << c << endl;
-    
-    //cout << " a * b = " << a << " * " << b << " = " << a * b << endl;
-    
+	
     clsPlano3D q1,q2,q3;
-    
     
     
     a.x( 3.0 );
@@ -205,12 +373,17 @@ int main()
     q1=clsPlano3D(a, b, c);
     
     cout << q1;
-//    cout << q1 << endl;
-    
-//    cout << "Interseccion " << interseccion( q1,q2,q3 ) << endl;
-    
+
+    istrstream plano1("1*x-3*y+2*z+3=0");
+    istrstream plano2("5*x+6*y-1*z-13=0");
+    istrstream plano3("4*x-1*y+3*z-8=0");
+    plano1>>q1;
+    plano2>>q2;
+    plano3>>q3;
+    d=interseccion(q1,q2,q3);
+    cout<<d<<endl;
+	
     return 0;
     
 }
 // ========================================================
-
